1ff59de532eac9c865aae3ea5de3e8a4dc1b6e51
diak <diak@mesterlevel.lo>
2025-02-11 13:29:41 +0100

    változtatás szövege

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // telepített csomagok importálása
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path');
