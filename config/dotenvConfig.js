c2f0907abef67e21fcf367a3e153250cbc06b04e
diak <diak@mesterlevel.lo>
2025-02-26 10:08:24 +0100

    server.js, dotenvConfig.js, .env

--- config/dotenvConfig.js
+++ config/dotenvConfig.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const config = {
+    PORT: process.env.PORT,
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const { PORT } = require('./config').config;
 
-app.listen( () => {
+app.listen(PORT, () => {
     console.log(` https://nodejs312.dszcbaross.edu.hu`);
 });
