a4b38f725fe82a98716ae11c04280327f16599ad
diak <diak@mesterlevel.lo>
2025-03-28 11:34:15 +0100

    Controllers

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -7,31 +7,35 @@ const takeProduct = (req, res) => {
     const { product_id} = req.params;
     const {quantity}=req.body;
     const sql2="INSERT IGNORE INTO cart (cart_id, user_id) VALUES (?, ?);";
+    const sql5="SELECT * FROM products WHERE product_id=?;";
+    const sql="INSERT INTO `cart_items` (`cart_item_id`, `cart_id`, `product_id`, `quantity`, `cat_id`) VALUES (NULL,?,?,?,?)";
     db.query(sql2, [cart_id,userid], (err, result) => {
         if (err) {
             return res.status(500).json({ error: 'Hiba az SQL-ben' });
         }
-        //return res.status(200).json({ message: 'Sikeresen letrehoztad a kosarad! ' });
-        const sql5="SELECT * FROM products WHERE product_id=?;"
-        db.query(sql5,[product_id],(err,result)=>{
-        if(err){
-            return res.status(500).json({error:"SQL Hiba" + err})
-        }
-        if(result.lenght===0){
-            return res.status(404).json({error:'Nincs ilyen termék!'});
-        }
-        if(result>0){
-            const catid=result.cat_id;
-            const sql="INSERT INTO `cart_items` (`cart_item_id`, `cart_id`, `product_id`, `quantity`, `cat_id`) VALUES (NULL,?,?,?,?)";
-                db.query(sql, [cart_id,product_id,quantity,catid], (err, result) => {
-                if (err) {
-                    return res.status(500).json({ error: 'Hiba az SQL-ben' });
+        else{
+                db.query(sql5,[product_id],(err,result)=>{
+                if(err){
+                    return res.status(500).json({error:"SQL Hiba" + err})
                 }
-                return res.status(200).json({ message: 'Sikeresen frissítetted a kosaradat! ' });
-        });
-        
-        }
-    })
+                if(result.lenght===0){
+                    return res.status(404).json({error:'Nincs ilyen termék!'});
+                }
+                if(result>0){
+                    const catid=result.cat_id;
+                        db.query(sql, [cart_id,product_id,quantity,catid], (err, result) => {
+                        if (err) {
+                            return res.status(500).json({ error: 'Hiba az SQL-ben' });
+                        }
+                    return res.status(200).json({ message: 'Sikeresen frissítetted a kosaradat! ' });
+                    });
+               
+                }
+                })
+            }
+            
+        //return res.status(200).json({ message: 'Sikeresen letrehoztad a kosarad! ' });
+     
     });
     
     
