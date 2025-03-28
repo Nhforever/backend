778b2b32ab44fa2b614f61f4e4b0607290953ba4
diak <diak@mesterlevel.lo>
2025-03-28 11:28:16 +0100

    Controllers

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -4,18 +4,16 @@ const db = require('../models/db');
 const takeProduct = (req, res) => {
     const userid=req.user.id;
     const cart_id=userid+100;
+    const { product_id} = req.params;
+    const {quantity}=req.body;
     const sql2="INSERT IGNORE INTO cart (cart_id, user_id) VALUES (?, ?);";
     db.query(sql2, [cart_id,userid], (err, result) => {
         if (err) {
             return res.status(500).json({ error: 'Hiba az SQL-ben' });
         }
-        return res.status(200).json({ message: 'Sikeresen letrehoztad a kosarad! ' });
-    });
-    
-    const { product_id} = req.params;
-    const {quantity}=req.body;
-    const sql5="SELECT * FROM products WHERE product_id=?;"
-    db.query(sql5,[product_id],(err,result)=>{
+        //return res.status(200).json({ message: 'Sikeresen letrehoztad a kosarad! ' });
+        const sql5="SELECT * FROM products WHERE product_id=?;"
+        db.query(sql5,[product_id],(err,result)=>{
         if(err){
             return res.status(500).json({error:"SQL Hiba" + err})
         }
@@ -33,6 +31,10 @@ const takeProduct = (req, res) => {
         return res.status(200).json({ message: 'Sikeresen frissítetted a kosaradat! ' });
         }
     })
+    });
+    
+    
+    
     
 };
 //termék kosárbol kitörlése és a kosár törlése
