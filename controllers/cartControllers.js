a01922f6fd2613b89b6e3dc5ffbc2e39029ca1d3
diak <diak@mesterlevel.lo>
2025-03-28 11:21:48 +0100

    Controllers

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -14,7 +14,7 @@ const takeProduct = (req, res) => {
     
     const { product_id} = req.params;
     const {quantity,cat_id}=req.body;
-    const sql5="SELECT * FROM products WHERE prduct_id=?;"
+    const sql5="SELECT * FROM products WHERE product_id=?;"
     db.query(sql5,[product_id],(err,result)=>{
         if(err){
             return res.status(500).json({error:"SQL Hiba" + err})
