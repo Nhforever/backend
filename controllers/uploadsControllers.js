a13aa0c0048dd0a7273c4fec2933cc487f0bc88b
diak <diak@mesterlevel.lo>
2025-03-31 12:07:11 +0200

    Controllers

--- controllers/uploadsControllers.js
+++ controllers/uploadsControllers.js
@@ -39,11 +39,11 @@ const uploadProduct = (req, res) => {
 };
 //előre összeállított gép feltöltése
 const uploadConfig=(req, res) => {
-    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,cpu_cooler,price,in_stock,cat_id,sale,sale_,config_name,description,active } = req.body;
+    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,cpu_cooler,price,in_stock,sale,sale_,config_name,description,active } = req.body;
 
     const config_pic = req.file ? req.file.filename : null;
 
-    const sql = "INSERT INTO pc_configs (pc_id, cpu, mother_board, house, ram, gpu, hdd, ssd, power_supply,cpu_cooler, price, in_stock, cat_id, sale,sale_, config_name, config_pic, description,active)VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?);";
+    const sql = "INSERT INTO pc_configs (pc_id, cpu, mother_board, house, ram, gpu, hdd, ssd, power_supply,cpu_cooler, price, in_stock, cat_id, sale,sale_, config_name, config_pic, description,active)VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 1, ?, ?, ?,?,?,?);";
     db.query(sql, [cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,cpu_cooler,price,in_stock,cat_id,sale,sale_,config_name,config_pic,description,active], (err, result) => {
         if (err) {
             console.error('SQL Hiba:', err);
