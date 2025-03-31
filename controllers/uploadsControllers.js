8295630674f67d84e1ddae15813431ba390768cb
diak <diak@mesterlevel.lo>
2025-03-31 10:19:36 +0200

    Controllers

--- controllers/uploadsControllers.js
+++ controllers/uploadsControllers.js
@@ -15,13 +15,14 @@ const uploadCategory=(req, res) => {
 const uploadProduct = (req, res) => {
     const { product_name, price, in_stock, cat_id, sale,sale_,product_description
     } = req.body;
+    const product_pic = req.file ? req.file.filename : null;
     console.log("név: "+product_name, "típus: "+typeof(product_name)+"\nprice: "+price, "típus: "+typeof(price)+"\ninstock: "+in_stock, "típus: "+typeof(in_stock)+"\ncatid: "+cat_id, "típus: "+typeof(cat_id)+"\nsale: "+sale, "típus: "+typeof(sale)+"\npic: "+product_pic, "típus: "+typeof(product_pic)+"\n");
     // Alap validálás
     if (!product_name || !price || !in_stock || !cat_id || !sale ||!product_description) {
         return res.status(400).json({ error: 'Minden mező kitöltése kötelező!' });
     }
 
-    const product_pic = req.file ? req.file.filename : null;
+    
 
     const sql = "INSERT INTO `products` (`product_id`, `product_name`, `price`, `in_stock`, `cat_id`, `sale`, `product_pic`,sale_,product_description) VALUES (NULL, ?, ?, ?, ?, ?, ?,?,?)";
     db.query(sql, [product_name, parseFloat(price), parseInt(in_stock, 10), parseInt(cat_id, 10), parseInt(sale, 10), product_pic,sale_,product_description], (err, result) => {
