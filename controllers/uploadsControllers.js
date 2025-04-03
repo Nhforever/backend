26719ddcca8fa54209a9395299c954e9aa2b8b47
diak <diak@mesterlevel.lo>
2025-04-03 10:57:14 +0200

    Controllers

--- controllers/uploadsControllers.js
+++ controllers/uploadsControllers.js
@@ -13,7 +13,7 @@ const uploadCategory=(req, res) => {
 };
 //termék feltöltése
 const uploadProduct = (req, res) => {
-    const { product_name, price, in_stock, cat_id, sale,sale_,product_description
+    const { product_name, price, in_stock, cat_id, sale,sale_,description
     } = req.body;
     const product_pic = req.file ? req.file.filename : null;
     if(product_pic===null){
@@ -21,14 +21,14 @@ const uploadProduct = (req, res) => {
     }
     console.log("név: "+product_name, "típus: "+typeof(product_name)+"\nprice: "+price, "típus: "+typeof(price)+"\ninstock: "+in_stock, "típus: "+typeof(in_stock)+"\ncatid: "+cat_id, "típus: "+typeof(cat_id)+"\nsale: "+sale, "típus: "+typeof(sale)+"\npic: "+product_pic, "típus: "+typeof(product_pic)+"\n");
     // Alap validálás
-    if (!product_name || !price || !in_stock || !cat_id || !sale ||!product_description) {
+    if (!product_name || !price || !in_stock || !cat_id || !sale ||!description) {
         return res.status(400).json({ error: 'Minden mező kitöltése kötelező!' });
     }
 
     
 
-    const sql = "INSERT INTO `products` (`product_id`, `product_name`, `price`, `in_stock`, `cat_id`, `sale`, `product_pic`,sale_,product_description) VALUES (NULL, ?, ?, ?, ?, ?, ?,?,?)";
-    db.query(sql, [product_name, parseFloat(price), parseInt(in_stock, 10), parseInt(cat_id, 10), parseInt(sale, 10), product_pic,sale_,product_description], (err, result) => {
+    const sql = "INSERT INTO `products` (`product_id`, `product_name`, `price`, `in_stock`, `cat_id`, `sale`, `product_pic`,sale_,description) VALUES (NULL, ?, ?, ?, ?, ?, ?,?,?)";
+    db.query(sql, [product_name, parseFloat(price), parseInt(in_stock, 10), parseInt(cat_id, 10), parseInt(sale, 10), product_pic,sale_,description], (err, result) => {
         if (err) {
             console.error('SQL Hiba:', err);
             return res.status(500).json({ error: 'Hiba az SQL-ben', details: err });
