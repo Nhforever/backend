cb59f0bcba6d4d32fa8255a621a7c9e61a3b35a3
diak <diak@mesterlevel.lo>
2025-04-03 11:07:10 +0200

    Controllers

--- controllers/editControllers.js
+++ controllers/editControllers.js
@@ -19,11 +19,11 @@ const editProduct = (req, res) => {
 };
 const editConfig=(req, res) => {
     const product_id=req.params.product_id;
-    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,price,cpu_cooler,in_stock,cat_id,sale,sale_,config_name,description,active } = req.body;
+    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,price,cpu_cooler,in_stock,cat_id,sale,sale_,product_name,description,active } = req.body;
 
-    const config_pic = req.file ? req.file.filename : null;
-    const sql = 'UPDATE pc_configs SET cpu=?,mother_board=?,house=?,ram=?,gpu=?,hdd=?,ssd=?,power_supply=?,price=?,cpu_cooler=?,in_stock=?,cat_id=?,sale=?,sale_=?,config_name=?,config_pic=COALESCE(NULLIF(?, ""), config_pic),description=?,active=? WHERE product_id=?;'
-    db.query(sql,[cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,price,cpu_cooler,in_stock,cat_id,sale,sale_,config_name,config_pic,description,active,product_id], (err, result) => {
+    const product_pic = req.file ? req.file.filename : null;
+    const sql = 'UPDATE pc_configs SET cpu=?,mother_board=?,house=?,ram=?,gpu=?,hdd=?,ssd=?,power_supply=?,price=?,cpu_cooler=?,in_stock=?,cat_id=?,sale=?,sale_=?,product_name=?,product_pic=COALESCE(NULLIF(?, ""), product_pic),description=?,active=? WHERE product_id=?;'
+    db.query(sql,[cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,price,cpu_cooler,in_stock,cat_id,sale,sale_,product_name,product_pic,description,active,product_id], (err, result) => {
         if (err) {
             console.error('SQL Hiba:', err);
             return res.status(500).json({ error: 'Hiba az SQL-ben', details: err });
--- controllers/uploadsControllers.js
+++ controllers/uploadsControllers.js
@@ -40,12 +40,12 @@ const uploadProduct = (req, res) => {
 //előre összeállított gép feltöltése
 const uploadConfig=(req, res) => {
     const cat_id=1;
-    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,cpu_cooler,price,in_stock,sale,sale_,config_name,description,active } = req.body;
+    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,cpu_cooler,price,in_stock,sale,sale_,product_name,description,active } = req.body;
 
-    const config_pic = req.file ? req.file.filename : null;
+    const product_pic = req.file ? req.file.filename : null;
 
-    const sql = "INSERT INTO pc_configs (product_id, cpu, mother_board, house, ram, gpu, hdd, ssd, power_supply,cpu_cooler, price, in_stock, cat_id, sale,sale_, config_name, config_pic, description,active)VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?);";
-    db.query(sql, [cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,cpu_cooler,price,in_stock,cat_id,sale,sale_,config_name,config_pic,description,active], (err, result) => {
+    const sql = "INSERT INTO pc_configs (product_id, cpu, mother_board, house, ram, gpu, hdd, ssd, power_supply,cpu_cooler, price, in_stock, cat_id, sale,sale_, product_name, product_pic, description,active)VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?);";
+    db.query(sql, [cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,cpu_cooler,price,in_stock,cat_id,sale,sale_,product_name,product_pic,description,active], (err, result) => {
         if (err) {
             console.error('SQL Hiba:', err);
             return res.status(500).json({ error: 'Hiba az SQL-ben', details: err });
