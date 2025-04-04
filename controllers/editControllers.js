decefe6faac6a88334834e800a0a70013b9e602c
diak <diak@mesterlevel.lo>
2025-04-04 10:16:33 +0200

    Controllers

--- controllers/editControllers.js
+++ controllers/editControllers.js
@@ -19,11 +19,11 @@ const editProduct = (req, res) => {
 };
 const editConfig=(req, res) => {
     const pc_id=req.params.product_id;
-    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,price,cpu_cooler,in_stock,cat_id,sale,sale_,pc_name,pc_description,active } = req.body;
+    const { cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,pc_price,cpu_cooler,in_stock,cat_id,sale,sale_,pc_name,pc_description,active } = req.body;
 
     const pc_pic = req.file ? req.file.filename : null;
-    const sql = 'UPDATE pc_configs SET cpu=?,mother_board=?,house=?,ram=?,gpu=?,hdd=?,ssd=?,power_supply=?,price=?,cpu_cooler=?,in_stock=?,cat_id=?,sale=?,sale_=?,product_name=?,product_pic=COALESCE(NULLIF(?, ""), product_pic),description=?,active=? WHERE product_id=?;'
-    db.query(sql,[cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,price,cpu_cooler,in_stock,cat_id,sale,sale_,pc_name,pc_pic,pc_description,active,pc_id], (err, result) => {
+    const sql = 'UPDATE pc_configs SET cpu=?,mother_board=?,house=?,ram=?,gpu=?,hdd=?,ssd=?,power_supply=?,pc_price=?,cpu_cooler=?,in_stock=?,cat_id=?,sale=?,sale_=?,product_name=?,product_pic=COALESCE(NULLIF(?, ""), product_pic),description=?,active=? WHERE product_id=?;'
+    db.query(sql,[cpu,mother_board,house,ram,gpu,hdd,ssd,power_supply,pc_price,cpu_cooler,in_stock,cat_id,sale,sale_,pc_name,pc_pic,pc_description,active,pc_id], (err, result) => {
         if (err) {
             console.error('SQL Hiba:', err);
             return res.status(500).json({ error: 'Hiba az SQL-ben', details: err });
