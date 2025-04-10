cb67d4c48b23192f3d8f2bf883e4f6afc7e876bf
diak <diak@mesterlevel.lo>
2025-04-10 10:17:12 +0200

    Controllers

--- routes/uploadsRoutes.js
+++ routes/uploadsRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/uploadCategory', authenticateToken,adminMiddleware, uploadCategory);
 router.post('/uploadProduct',authenticateToken, upload.single('product_pic'),adminMiddleware,uploadProduct);
-router.post('/uploadConfig', authenticateToken,upload.single('config_pic') ,adminMiddleware,uploadConfig);
+router.post('/uploadConfig', authenticateToken,upload.single('pc_pic') ,adminMiddleware,uploadConfig);
 
 
 module.exports = router;
