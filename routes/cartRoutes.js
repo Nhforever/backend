11a310f9e2ea1561e0b21121e52eea86b023b0d7
diak <diak@mesterlevel.lo>
2025-04-11 11:08:19 +0200

    readme

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authenticateToken = require('../middleware/jwtAuth');
-const { takeProduct,RemoveProduct, ShowCart, SUMprice } = require('../controllers/cartControllers');
+const { takeProduct,RemoveProduct, ShowCart, SUMprice,editQuantity } = require('../controllers/cartControllers');
 const router = express.Router();
 
 router.post('/takeProduct/:product_id',authenticateToken, takeProduct);
 router.delete('/removeProduct/:cart_item_id',authenticateToken, RemoveProduct);
 router.get('/myCart',authenticateToken,ShowCart);
-router.get('/sumPrice',authenticateToken,SUMprice)
+router.get('/sumPrice',authenticateToken,SUMprice);
+router.put('/editQuantity',authenticateToken,editQuantity);
 
 module.exports = router;
